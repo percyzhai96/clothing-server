7663fd6b8d8db2f2fd39ca30524055dbe4a35f6d
zhaipengju <3045908145@qq.com>
2025-04-07 15:45:59 +0800

    fix:submit

--- models/sys-goods.model.js
+++ models/sys-goods.model.js
@@ -2,34 +2,41 @@ const { DataTypes } = require("sequelize/types/index.js")
 
 module.exports = (sequelize, Sequelize) => {
   return sequelize.define('sys_goods', {
+    // 主键id
     id: {
       type: DataTypes.UUID,
       notNull: true,
       primaryKey: true,
       defaultValue: Sequelize.UUIDV4 // 或 DataTypes.UUIDV1
     },
+    // 进货方名称
     shopName: {
       type: DataTypes.STRING,
       notNull: true,
     },
+    // 进货时间
     purchaseTime: {
       type: DataTypes.DATEONLY,
       defaultValue: DataTypes.NOW
     },
+    // 商品款号
     styleNumber: {
       type: DataTypes.SMALLINT,
       notNull: true
     },
+    // 商品名称
     goodName: {
       type: DataTypes.STRING,
       notNull: true,
       defaultValue: "",
     },
+    // 商品数量
     goodNum: {
       type: DataTypes.INTEGER,
       notNull: true,
       defaultValue: 1,
     },
+    // 商品单价
     unitPrice: {
       type: DataTypes.DECIMAL(10, 2),
       notNull: true,
