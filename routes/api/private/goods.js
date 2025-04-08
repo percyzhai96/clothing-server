7c3e51d737155697705924722d7621ba25d1c15f
zhaipengju <3045908145@qq.com>
2025-04-08 15:46:54 +0800

    fix:更改使接口更加规范

--- routes/api/private/goods.js
+++ routes/api/private/goods.js
@@ -3,8 +3,9 @@ const router = express.Router()
 const Goods = require("../../../controllers/sys-goods.controller")
 /**
   * 新增进货商品
-  * @route POST /api/private/goodsCreate
+  * @route POST /api/private/goods/create
   * @group 商品管理
  */
-router.post("/goodsCreate", Goods.create);
+router.post("/goods/create", Goods.create)
 
+module.exports = router
